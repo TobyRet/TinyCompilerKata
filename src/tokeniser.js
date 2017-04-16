daa2a857cf5b970803872b1d2214c20658ca4369
tobyret <toby.retallick@gmail.com>
2017-04-16 22:35:08 +0100

    Tokeniser unit test - recognising numbers

--- src/tokeniser.js
+++ src/tokeniser.js
@@ -1,10 +1,13 @@
 export function tokeniser(input) {
   return input.split('').map(char => {
     if(char === '(') {
-      return {type: 'paren', value: '('}
+      return {type: 'paren', value: char}
     }
     if(char === ')') {
-      return {type: 'paren', value: ')'}
+      return {type: 'paren', value: char}
+    }
+    if(/[0-9]/.test(char)) {
+      return {type: 'number', value: char}
     }
   })
 }
--- test/tokeniser_spec.js
+++ test/tokeniser_spec.js
@@ -2,13 +2,14 @@ import {tokeniser} from "../src/tokeniser";
 import {expect} from "chai";
 
 describe('Tokeniser', () => {
-  describe('Parenthesis', ()=> {
+  describe('Parenthesis', () => {
     it('recognises a left parenthesis', () => {
       const input = '('
       const expectedToken = [{
         type: 'paren',
         value: '('
       }]
+
       expect(tokeniser(input)).to.eql(expectedToken)
     })
 
@@ -18,6 +19,7 @@ describe('Tokeniser', () => {
         type: 'paren',
         value: ')'
       }]
+
       expect(tokeniser(input)).to.eql(expectedToken)
     })
 
@@ -33,6 +35,41 @@ describe('Tokeniser', () => {
           value: ')'
         }
       ]
+
+      expect(tokeniser(input)).to.eql(expectedToken)
+    })
+  })
+  describe('Numbers', () => {
+    it('recognises numbers from 0-9', () => {
+      const input = '1'
+      const expectedToken = [{
+        type: 'number',
+        value: '1'
+      }]
+
+      expect(tokeniser(input)).to.eql(expectedToken)
+    })
+    it('recognises numbers from 0-9 and parenthesis', () => {
+      const input = '(29)'
+      const expectedToken = [
+        {
+          type: 'paren',
+          value: '('
+        },
+        {
+          type: 'number',
+          value: '2'
+        },
+        {
+          type: 'number',
+          value: '9'
+        },
+        {
+          type: 'paren',
+          value: ')'
+        }
+      ]
+
       expect(tokeniser(input)).to.eql(expectedToken)
     })
   })
